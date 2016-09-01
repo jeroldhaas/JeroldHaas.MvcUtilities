1a7ee586cefd93dc3b03796302a27b726dc01464
Jerold Haas <jeroldhaas@gmail.com>
2016-09-01 18:38:42 -0400

    initial commit

---
+++ JeroldHaas.MvcUtilities/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JeroldHaas.MvcUtilities
+{
+    public static class Controller
+    {
+        public static string nameofcontroller(Type t)
+        {
+            var name = nameof(t);
+            if (name.Contains("Controller"))
+            {
+                return name.Substring(
+                    0,
+                    name.IndexOf(@"Controller") - 1
+                );
+            }
+            else return "";
+        }
+    }
+}
